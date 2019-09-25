c934363f650d75bb54bdc842f9a57d2f283c1bdf
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-09-25 20:05:45 +0700

    Add files via upload

---
+++ PBOULG-10115557-Latihan10-Logika/src/pboulg/pkg10115557/latihan10/logika/PBOULG10115557Latihan10Logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan10.logika;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan 10
+ */
+public class PBOULG10115557Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+	int umur = 19;
+	boolean dewasa;
+	dewasa = (umur >= 17);
+	System.out.println("Status Dewasa : " + dewasa);
+	}
+    
+}
